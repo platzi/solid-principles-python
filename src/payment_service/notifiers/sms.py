4cec598eeb624770e4be142e00299e7a0809e506
Eduardo Alvarez <eduardo.alvarez@protonmail.com>
2024-10-08 15:16:32 -0500

    feat: inicio del modulo del servicio de pagos

---
+++ src/payment_service/notifiers/sms.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+from payment_service.commons import CustomerData
+
+
+@dataclass
+class SMSNotifier:
+    gateway: str
+
+    def send_confirmation(self, customer_data: CustomerData):
+        phone_number = customer_data.contact_info.phone
+        if not phone_number:
+            print("No phone number provided")
+            return
+        print(
+            f"SMS sent to {phone_number} via {self.gateway}: Thank you for your payment."
+        )
