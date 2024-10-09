9073dfa57a7fea312f3e13c5b47dee636a1e2497
Eduardo Alvarez <eduardo.alvarez@protonmail.com>
2024-10-09 09:36:53 -0500

    fix: method name

--- src/payment_service/processors/offline_processor.py
+++ src/payment_service/processors/offline_processor.py
@@ -3,7 +3,7 @@ from payment_service.commons import CustomerData, PaymentData, PaymentResponse
 
 
 class OfflinePaymentProcessor(PaymentProcessorProtocol):
-    def process_payment(
+    def process_transaction(
         self, customer_data: CustomerData, payment_data: PaymentData
     ) -> PaymentResponse:
         print("Processing offline payment for", customer_data.name)
